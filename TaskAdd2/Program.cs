9528b9f830914d4a6f8ce8be066fc413f7491060
Ekyd <Ekyd777@gmail.com>
2022-04-26 21:01:21 +0300

    add taskadd2

---
+++ TaskAdd2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Есть программа с бесконечным циклом. Когда пользователь вводит exit программа заканчивается
+Console.WriteLine("Тест на сообразительность!");
+Console.WriteLine("Ответе на вопрос");
+string exitWord = "exit";
+string? userUnswer = string.Empty;
+while (exitWord != userUnswer)
+{
+    Console.Write("Введите ответ: ");
+    userUnswer = Console.ReadLine();
+    if (userUnswer == exitWord)
+    {
+        Console.WriteLine("А ты сообразительный, тест завершен");
+        break;
+    }
+    else
+    {
+      Console.WriteLine("Не верно, повторите попытку");  
+    }
+}
